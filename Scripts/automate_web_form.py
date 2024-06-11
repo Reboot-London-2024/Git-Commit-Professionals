e268507e07b79c14f979503ede06a5195d879043
cw2-mackintosh <connor2.mackintosh@live.uwe.ac.uk>
2024-06-11 11:34:13 +0100

    v1 script finished

--- Scripts/automate_web_form.py
+++ Scripts/automate_web_form.py
@@ -1,22 +1,64 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import chrome
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
 
-options = webdriver.ChromeOptions()
 
-options.add_experimental_option('detach', True)
+chrome_options = Options() 
+chrome_options.add_experimental_option("detach", True)
 
-driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome(options=chrome_options)
 
-# Navigate to the web form URL
-driver.get('http://127.0.0.1:5500/forms.html')
 
-sname = 'Connor Mackintosh'
-name = driver.find_element('xpath', '//*[@id="name"]' )
+# Open the web form URL
+driver.get("http://127.0.0.1:5500/forms.html")
 
-# Fill in the form fields using XPath
-name.send_keys(sname)
+fname = 'Connor Mackintosh'
+femployee_number = '510956'
+fcost_centre = '400600'
+ftype_of_request = 'Hardware'
+fjustification = 'I am a new software engineer and I need advanced web access for developer resources'
+name = driver.find_element('xpath', '//*[@id="name"]')
+employee_number = driver.find_element('xpath', '//*[@id="employee-number"]')
+cost_center = driver.find_element('xpath', '//*[@id="cost-centre"]')
+type_of_request = driver.find_element('xpath', '//*[@id="type-of-request"]')
+justification = driver.find_element('xpath', '//*[@id="justification"]')
+name.send_keys(fname)
+employee_number.send_keys(femployee_number)
+cost_center.send_keys(fcost_centre)
+justification.send_keys(fjustification)
+type_of_request.send_keys(ftype_of_request)
 
-# Close the browser
-driver.quit()
+
+# New Url 
+new_url = "http://127.0.0.1:5500/forms2.html"
+
+# Open a new window 
+driver.execute_script("window.open('');") 
+  
+# Switch to the new window and open new URL 
+driver.switch_to.window(driver.window_handles[1]) 
+driver.get(new_url) 
+
+
+fcloud_tenant = 'CNEAzure'
+fengineering_lead = "Joe Bloggs"
+fcloud_platform = "GCP"
+
+name = driver.find_element('xpath', '//*[@id="name"]')
+employee_number = driver.find_element('xpath', '//*[@id="employee-number"]')
+cost_center = driver.find_element('xpath', '//*[@id="cost-centre"]')
+type_of_request = driver.find_element('xpath', '//*[@id="type-of-request"]')
+justification = driver.find_element('xpath', '//*[@id="justification"]')
+cloud_tenant = driver.find_element('xpath', '//*[@id="cloud-tenant"]')
+engineering_lead = driver.find_element('xpath', '//*[@id="engineering-lead"]')
+cloud_platform = driver.find_element('xpath', '//*[@id="cloud-platform"]')
+name.send_keys(fname)
+employee_number.send_keys(femployee_number)
+cost_center.send_keys(fcost_centre)
+justification.send_keys(fjustification)
+type_of_request.send_keys(ftype_of_request)
+cloud_tenant.send_keys(fcloud_tenant)
+engineering_lead.send_keys(fengineering_lead)
+cloud_platform.send_keys(fcloud_platform)
